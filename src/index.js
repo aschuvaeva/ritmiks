eb511dba965e022429f5a86a7adfb2d493e35ed5
aschuvaeva_11 <aschuvaeva_1@edu.hse.ru>
2024-08-03 15:22:45 +0300

    all

--- src/index.js
+++ src/index.js
@@ -190,120 +190,120 @@ app.post('/submit_freest', (req, res) => {
   });
 
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  // app.use(bodyParser.json());
   
-  const modelPathS = 'public/ritmiks_plan_model/model.json';
-  let model;
+  // const modelPathS = 'public/ritmiks_plan_model/model.json';
+  // let model;
   
-  async function loadModel() {
-    if (!model) {
-        model = await tf.loadLayersModel(`file://${modelPathS}`);
-    }
-  }
+  // async function loadModel() {
+  //   if (!model) {
+  //       model = await tf.loadLayersModel(`file://${modelPathS}`);
+  //   }
+  // }
 
-  app.post('/process_form', async (req, res) => {
-    const direction = req.body.direction;
-    const pushups = parseInt(req.body.pushups);
-    const squats = parseInt(req.body.squats);
-    const abs = parseInt(req.body.abs);
-    const flexibility = parseInt(req.body.flexibility);
+  // app.post('/process_form', async (req, res) => {
+  //   const direction = req.body.direction;
+  //   const pushups = parseInt(req.body.pushups);
+  //   const squats = parseInt(req.body.squats);
+  //   const abs = parseInt(req.body.abs);
+  //   const flexibility = parseInt(req.body.flexibility);
 
 
-    // Загрузка модели
-    await loadModel();
+  //   // Загрузка модели
+  //   await loadModel();
 
-    // Подготовка данных для модели
-    const inputData = tf.tensor2d([[squats, pushups, abs, flexibility]]);
+  //   // Подготовка данных для модели
+  //   const inputData = tf.tensor2d([[squats, pushups, abs, flexibility]]);
 
-    // Получение предсказаний от модели
-    const outputData = model.predict(inputData);
+  //   // Получение предсказаний от модели
+  //   const outputData = model.predict(inputData);
 
-    // Округление предсказанных значений до целого
-    const predictions = outputData.arraySync()[0].map(value => Math.round(value));
+  //   // Округление предсказанных значений до целого
+  //   const predictions = outputData.arraySync()[0].map(value => Math.round(value));
 
     // Определение упражнений и их предсказанных значений
-    const exercises = [
-        'Отжимания с колен',
-        'Отжимания',
-        'Отжимания узкие',
-        'Стульчик',
-        'Приседания',
-        'Приседания с прыжком',
-        'Планка',
-        'Скручивания',
-        'Складки',
-        'Растяжка в складочке',
-        'Растяжка в стредле',
-        'Шпагаты'
-    ];
+//     const exercises = [
+//         'Отжимания с колен',
+//         'Отжимания',
+//         'Отжимания узкие',
+//         'Стульчик',
+//         'Приседания',
+//         'Приседания с прыжком',
+//         'Планка',
+//         'Скручивания',
+//         'Складки',
+//         'Растяжка в складочке',
+//         'Растяжка в стредле',
+//         'Шпагаты'
+//     ];
 
-    const exercisesWithPredictions = exercises.map((exercise, index) => {
-      let time;
-      switch (exercise) {
-          case 'Отжимания с колен':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Отжимания':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Отжимания узкие':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Стульчик':
-              time = `${predictions[index]} минуты`;
-              break;
-          case 'Приседания':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Приседания с прыжком':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Планка':
-              time = `${predictions[index]} минута`;
-              break;
-          case 'Скручивания':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Складки':
-              time = `${predictions[index]} раз`;
-              break;
-          case 'Растяжка в складочке':
-              time = `${predictions[index]} секунд`;
-              break;
-          case 'Растяжка в стредле':
-              time = `${predictions[index]} минуты`;
-              break;
-          case 'Шпагаты':
-              time = `${predictions[index]} минуты`;
-              break;
-          default:
-              time = 0;
-      }
-      return `${exercise}: ${time}`;
-  });
+//     const exercisesWithPredictions = exercises.map((exercise, index) => {
+//       let time;
+//       switch (exercise) {
+//           case 'Отжимания с колен':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Отжимания':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Отжимания узкие':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Стульчик':
+//               time = `${predictions[index]} минуты`;
+//               break;
+//           case 'Приседания':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Приседания с прыжком':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Планка':
+//               time = `${predictions[index]} минута`;
+//               break;
+//           case 'Скручивания':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Складки':
+//               time = `${predictions[index]} раз`;
+//               break;
+//           case 'Растяжка в складочке':
+//               time = `${predictions[index]} секунд`;
+//               break;
+//           case 'Растяжка в стредле':
+//               time = `${predictions[index]} минуты`;
+//               break;
+//           case 'Шпагаты':
+//               time = `${predictions[index]} минуты`;
+//               break;
+//           default:
+//               time = 0;
+//       }
+//       return `${exercise}: ${time}`;
+//   });
   
 
-    let result;
-    if (direction === 'чирлидинг флаер') {
-        result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]]; 
-    } else if (direction === 'чирлидинг база') {
-        result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[2], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10]]; 
-    } else if (direction === 'фристайл') {
-        result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]];
-    } else if (direction === 'акробатика') {
-        result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[2], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]]; 
-    }
+//     let result;
+//     if (direction === 'чирлидинг флаер') {
+//         result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]]; 
+//     } else if (direction === 'чирлидинг база') {
+//         result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[2], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10]]; 
+//     } else if (direction === 'фристайл') {
+//         result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]];
+//     } else if (direction === 'акробатика') {
+//         result = [exercisesWithPredictions[0], exercisesWithPredictions[1], exercisesWithPredictions[2], exercisesWithPredictions[3], exercisesWithPredictions[4], exercisesWithPredictions[5], exercisesWithPredictions[6], exercisesWithPredictions[7], exercisesWithPredictions[8], exercisesWithPredictions[9], exercisesWithPredictions[10], exercisesWithPredictions[11]]; 
+//     }
 
-    // Фильтрация упражнений с предсказанным количеством равным 0
-    result = result.filter(exercise => {
-        const quantity = parseInt(exercise.split(': ')[1]);
-        return quantity !== 0;
-    });
+//     // Фильтрация упражнений с предсказанным количеством равным 0
+//     result = result.filter(exercise => {
+//         const quantity = parseInt(exercise.split(': ')[1]);
+//         return quantity !== 0;
+//     });
 
-    // Отправка результата на страницу
-    res.render('plan', { results: result });
-});
+//     // Отправка результата на страницу
+//     res.render('plan', { results: result });
+// });
 
 let port = 3001 
 app.listen(port, () => {
